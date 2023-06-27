65e2773ae7f59313f50c792ac30d6fc38eab4c62
TXT-89 <txt.hackathon@txts-mbp-5.attlocal.net>
2023-06-26 22:02:16 -0700

    LightBulbChallenge

---
+++ app.js
@@ -0,0 +1,7 @@
+const img=document.getElementById("img")
+function on(){
+    img.src="https://www.lightbulbs.com/catalog/product_line_photos/light_bulbs_beauty.jpg?123456"
+}
+function off(){
+    img.src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Gluehlampe_01_KMJ.png/640px-Gluehlampe_01_KMJ.png"
+}
